d53ff2c2516b35e899f3cba924740924897383a4
edna f <contato.ednaleite@gmail.com>
2020-09-19 20:47:42 -0300

    commit 19/09 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,26 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-<link rel="stylesheet" type="text/css" href="css/css_oficial.css">
-<link rel="stylesheet" type="text/css" href="css/media_querys.css">
-<!-- <script src="https://code.jquery.com/jquery-2.0.3.min.js"></script> -->
-<script src="OwlCarousel2-2.3.4/docs/assets/vendors/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-<script src="js/jquery.easypiechart.min.js"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="css/css_oficial.css">
+	<link rel="stylesheet" type="text/css" href="css/media_querys.css">
+	<!-- <script src="https://code.jquery.com/jquery-2.0.3.min.js"></script> -->
+
+	<!-- Stylesheets -->
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,400italic,300italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="OwlCarousel2-2.3.4/docs/assets/css/docs.theme.min.css">
+
+    <!-- Owl Stylesheets -->
+    <link rel="stylesheet" href="OwlCarousel2-2.3.4/docs/assets/owlcarousel/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="OwlCarousel2-2.3.4/docs/assets/owlcarousel/assets/owl.theme.default.min.css">
+  
+  	<!-- javascript -->
+    <script src="OwlCarousel2-2.3.4/docs/assets/vendors/jquery.min.js"></script>
+    <script src="OwlCarousel2-2.3.4/docs/assets/owlcarousel/owl.carousel.js"></script>
+    <script src="OwlCarousel2-2.3.4/docs/assets/vendors/highlight.js"></script>
+    <script src="OwlCarousel2-2.3.4/docs/assets/js/app.js"></script>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+	<script src="js/jquery.easypiechart.min.js"></script>
+
 
 
 			<nav class="wrap-menu">
@@ -95,36 +110,135 @@
 
 					<div class="div_lucros">
 
-						
-						<div class="circulo_lucros" style=""> 
-							<div class="teste" style="">
-							    <span class="chart" data-percent="50"></span>
-							    <div id="textos_circulo">
+						<div class="circulo_lucros"> 
+							<span class="chart" data-percent="50"></span>
+							<div class="textos_circulo">
+							    <div id="total_circulo">
+							        Total
+							    </div>
 
-							         <div id="total_circulo">
-							              	Total
-							         </div>
+							    <div id="valor_lucrosanuais">
+							        R$ 1.750.00
+							    </div>
 
-							         <div id="valor_lucrosanuais">
-							              	R$ 1.750.00
-							         </div>
-
-							         <div id="ano_lucros">
-							              	Ano de 2020
-							         </div>
-	        					</div>
-        					</div>
+							    <div id="ano_lucros">
+							     	Ano de 2020
+							    </div>
+	        				</div>
     					</div>	
-				
+
+    			
+    					<div class="valor_lucros">
+    						<div class="valor_lucros_reais">
+    							<div class="digitos">R$ 1.000.00</div>
+    							<div class="digitos">R$ 900.00</div>
+    							<div class="digitos">R$ 300.00</div>
+    						</div>
+    						<div class="hrs_lucros">
+    							<hr class="hrs">
+    							<hr class="hrs">
+    							<hr class="hrs">
+    						</div>
+    						<div class="periodo_lucros">
+    							<div class="periodo">Dia</div>
+    							<div class="periodo">Semana</div>
+    							<div class="periodo">Mês</div>
+    						</div>
+    					</div>
 
 					</div>	
 				</div>
 
 
 			</div>
+
+
+			<div class="funcoes">
+				<div class="fontesgeral fontes_topo">
+					Funções
+					<hr class="hr_produtos">
+				</div>
+			</div>
+
+
+
+			<div class="produtos fontesgeral fontes_topo">
+				<div>Produtos</div>
+				<hr class="hr_produtos">
+
+
+				<div style="max-width: 100%;">
+					<section id="demos" style="">
+					    <div class="row">
+					        <div class="">
+					          	<div class="owl-carousel owl-theme">
+
+						            <div class="item">
+							              <div id="imagem_produtos">
+							                <img src="imagens/modelo1.jpg" style="width: 100%;height: 100%;border-radius: 10px">
+							              </div>
+
+							              <div style="height: 50%;border-radius: 9px;">
+							                2
+							              </div>
+						            </div>
+
+						            <div class="item">
+							              <div id="imagem_produtos">
+							              	<img src="imagens/modelo1.jpg" style="width: 100%;height: 100%;border-radius: 10px">
+							              </div>
+
+							              <div style="height: 50%;border-radius: 9px;">
+							                2
+							              </div>
+						            </div>
+
+						            <div class="item">
+							              <div id="imagem_produtos">
+							              	<img src="imagens/modelo1.jpg" style="width: 100%;height: 100%;border-radius: 10px">
+		 
+							              </div>
+
+							              <div style="height: 50%;border-radius: 9px;">
+							                2
+							              </div>
+					            	</div>
+					          	</div>
+					          
+					        </div>
+					    </div>
+					</section>
+				</div>
+
+			</div>
 			
 
 			
+
+		  <script>
+            $(document).ready(function() {
+              var owl = $('.owl-carousel');
+              owl.owlCarousel({
+                margin: 11,
+                nav: true,
+                loop: true,
+                responsive: {
+                  0: {
+                    items: 2.9
+                  },
+                  500: {
+                    items: 4
+                  },
+                  700:{
+                  	items: 5
+                  },
+                  1000: {
+                    items: 8
+                  }
+                }
+              })
+            })
+          </script>
 
 
 
